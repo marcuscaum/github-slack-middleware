5681b64ebc1db16671d54db7b4984dd01747b5fe
marcuscaum <marcuslucas.jb@gmail.com>
2018-06-01 14:52:27 -0300

    Add a post for pull-requests on index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/pull-requests', (req, res) => {
+  console.log(req, res);
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
