f144b1b8813f39dc41013ffd317c90f2f766a557
marcuscaum <marcuslucas.jb@gmail.com>
2018-06-01 16:18:05 -0300

    Send hello world

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.post('/pull-requests', (req, res) => {
-  res.send(req.body);
-
-  fetch('https://hooks.slack.com/services/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', {
+  var options = {
+    uri: 'https://hooks.slack.com/services/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',
     method: 'POST',
-    body: { "text": "Hello newbies! "},
+    json: {
+      "text": "test"
+    }
+  };
+
+  request(options, (error, response, body) => {
+    console.log(error);
+    if (!error && response.statusCode == 200) {
+      console.log(body.id) // Print the shortened url.
+    }
   });
+
+  res.send(req.body);
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
