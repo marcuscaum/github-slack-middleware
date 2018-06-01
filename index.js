a05d4641ac1900ab2bae5569f847916aecdad30e
marcuscaum <marcuslucas.jb@gmail.com>
2018-06-01 15:10:57 -0300

    Fetch to slack api

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.use(express.json());
 
 app.post('/pull-requests', (req, res) => {
   res.send(req.body);
+
+  fetch('https://hooks.slack.com/services/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', {
+    method: 'POST',
+    body: { "text": "Hello newbies! "},
+  });
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
