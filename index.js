7ed5f05a4ce31fe2f1521aa9f7d905084fa26345
marcuscaum <marcuslucas.jb@gmail.com>
2018-06-01 15:09:02 -0300

    Body parser for json encode

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.post('/pull-requests', (req, res) => {
-  console.log(req, res);
+  res.send(req.body);
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
